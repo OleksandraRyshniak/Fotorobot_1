71c20a3718b122e13e9124028af6244e2768aa52
OleksandraRyshniak <oleksandraryshniak@gmail.com>
2025-05-21 14:12:01 +0300

    Update _19.05.py

--- _19.05.py
+++ _19.05.py
@@ -8,6 +8,15 @@ olemas={}
 objektid={}
 
 
+nagu=["pea1.png", "pea2.png", "pea3.png", "pea4.png", "pea5.png"]
+nagu_index=-1
+
+silmad=["silmad1.png"]
+silmad_index=-1
+
+juuksed=["juuksed1.png"]
+juuksed_index=-1
+
 def toggle_osa(nimi, fail, x, y):
     if olemas.get(nimi):
         canvas.delete(objektid[nimi])
@@ -19,15 +28,40 @@ def toggle_osa(nimi, fail, x, y):
         objektid[nimi]=canvas.create_image(x,y,image=tk_img)
         olemas[nimi]=True
 
+def toggle_nagu():
+    global nagu_index
+    nagu_index = (nagu_index + 1) % len(nagu)
+    fail = nagu[nagu_index].strip()
+    olemas["nagu"] = False
+    toggle_osa("nagu", fail, 200, 200)
+
+def toggle_juuksed():
+    global juuksed_index
+    juuksed_index = (juuksed_index + 1) % len(juuksed)
+    fail = juuksed[juuksed_index].strip()
+    olemas["juuksed"] = False
+    toggle_osa("juuksed", fail, 200, 200)
+
+
+def toggle_silmad():
+    global silmad_index
+    silmad_index = (silmad_index + 1) % len(silmad)
+    fail = silmad[silmad_index].strip()
+    olemas["silmad"] = False
+    toggle_osa("silmad", fail, 200, 200)
+
+
+
 def salvesta_nägu():
     failnimi=simpledialog.askstring("Salvestapilt", "Sisesta failinimi (ilma laiendita):")
     if not failnimi:
                 return
     lõpp_pilt= Image.new("RGBA", (400, 400), (255, 255, 255, 255))
-    for nimi in ["nägu", "silmad", "nina", "suu"]:
+    for nimi in ["nägu", "juuksed", "kulmud", "silmad", "nina", "suu"]:
         if olemas.get(nimi):
             failitee={
-                "nägu":"alus.png",
+                "nägu":"pea1.png",
+                "juuksed": "juuksed1.png",
                 "silmad":"silmad1.png",
                 "nina":"nina1.png",
                 "suu":"suu1.png"}.get(nimi)
@@ -65,9 +99,12 @@ toggle_osa("nägu", "alus.png", 200,200) #face first
 olemas["nägu"]=True
 
 ctk.CTkLabel(frame, text="Vali näoosad:", **seaded).pack(pady=10)
-ctk.CTkButton(frame, text="Silmad", command=lambda: toggle_osa("silmad", "alus.png", 200,200), **seaded).pack(pady=5)
-ctk.CTkButton(frame, text="Nina", command=lambda: toggle_osa("nina", "alus.png", 200,200), **seaded).pack(pady=5)
-ctk.CTkButton(frame, text="Suu", command=lambda: toggle_osa("suu", "alus.png", 200,200), **seaded).pack(pady=5)
+ctk.CTkButton(frame, text="Nägu", command=lambda: toggle_nagu(), **seaded).pack(pady=5)
+ctk.CTkButton(frame, text="Juuksed", command=lambda: toggle_juuksed(), **seaded).pack(pady=5)
+ctk.CTkButton(frame, text="Silmad", command=lambda: toggle_silmad(), **seaded).pack(pady=5)
+ctk.CTkButton(frame, text="Nina", command=lambda: toggle_osa("nina", "alus.png", 150,150), **seaded).pack(pady=5)
+ctk.CTkButton(frame, text="Suu", command=lambda: toggle_osa("suu", "alus.png", 150,150), **seaded).pack(pady=5)
+
 nupp=ctk.CTkButton(frame, text="Salvesta", command=salvesta_nägu, **seaded).pack(side="bottom", pady=5)
 
 frame_mus=ctk.CTkFrame(frame)
